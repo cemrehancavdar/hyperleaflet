115b65a3dd766ccc2b25da35dba5dd6e777e600b
cemrehancavdar <cemrehancavdar@hotmail.com>
2023-02-12 12:08:53 +0300

    feat: polyline and polygon types added, geometry creation moved to utils

---
+++ src/leaflet_utils.js
@@ -0,0 +1,42 @@
+import L from 'leaflet';
+import { setGeometryEvents } from './events';
+
+export default function createLeafletObject(row) {
+  const { geometry, popup, geometryType, id } = row;
+  const parsedGeometry = JSON.parse(geometry);
+  switch (geometryType) {
+    case 'Point': {
+      const marker = L.marker(parsedGeometry);
+      if (popup) {
+        marker.bindPopup(popup);
+      }
+      setGeometryEvents(marker, id);
+      return marker;
+    }
+    case 'LineString': {
+      const flippedGeometry = L.GeoJSON.coordsToLatLngs(parsedGeometry, 1);
+      const line = L.polyline(flippedGeometry);
+      if (popup) {
+        line.bindPopup(popup);
+      }
+      setGeometryEvents(line, id);
+      return line;
+    }
+    case 'Polygon': {
+      console.log(parsedGeometry);
+      const flippedGeometry = L.GeoJSON.coordsToLatLngs(parsedGeometry, 1);
+      console.log(flippedGeometry);
+      const polygon = L.polygon(flippedGeometry);
+      if (popup) {
+        polygon.bindPopup(popup);
+      }
+      setGeometryEvents(polygon, id);
+      return polygon;
+    }
+    default: {
+      // eslint-disable-next-line no-console
+      console.warn(`${geometryType} is not supported`);
+      return null;
+    }
+  }
+}
