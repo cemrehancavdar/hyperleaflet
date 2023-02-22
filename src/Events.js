5e2c5289d36a559eeadb0376183136eb6397225e
cemrehancavdar <cemrehancavdar@hotmail.com>
2023-02-22 23:28:23 +0300

    refactor: boosted map events added more detail note: bbox, center, will add more

--- src/Events.js
+++ src/Events.js
@@ -7,13 +7,17 @@ export default function initMapEvents(map) {
 
   map.on('zoomend', () => {
     const elem = document.getElementById('map');
-    const event = new CustomEvent('mapzoom', { detail: { zoom: map.getZoom() } });
+    const event = new CustomEvent('mapzoom', {
+      detail: { zoom: map.getZoom(), center: map.getCenter(), bbox: map.getBounds() },
+    });
     elem.dispatchEvent(event);
   });
 
   map.on('move', () => {
     const elem = document.getElementById('map');
-    const event = new CustomEvent('mapmove', { detail: { bbox: map.getBounds(), center: map.getCenter() } });
+    const event = new CustomEvent('mapmove', {
+      detail: { zoom: map.getZoom(), center: map.getCenter(), bbox: map.getBounds() },
+    });
     elem.dispatchEvent(event);
   });
 }
