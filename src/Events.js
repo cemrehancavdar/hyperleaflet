52bf445af8d30f771e340a99f6a7b7e654510a53
cemrehancavdar <cemrehancavdar@hotmail.com>
2023-02-24 00:07:49 +0300

    refactor: hyperleaflet events dispatches to window to work with alpine and other libraries

--- src/Events.js
+++ src/Events.js
@@ -1,31 +1,27 @@
 export default function initMapEvents(map) {
   map.on('click', (e) => {
-    const elem = document.getElementById('map');
     const event = new CustomEvent('mapclick', { detail: { point: e.latlng } });
-    elem.dispatchEvent(event);
+    window.dispatchEvent(event);
   });
 
   map.on('zoomend', () => {
-    const elem = document.getElementById('map');
     const event = new CustomEvent('mapzoom', {
       detail: { zoom: map.getZoom(), center: map.getCenter(), bbox: map.getBounds() },
     });
-    elem.dispatchEvent(event);
+    window.dispatchEvent(event);
   });
 
   map.on('move', () => {
-    const elem = document.getElementById('map');
     const event = new CustomEvent('mapmove', {
       detail: { zoom: map.getZoom(), center: map.getCenter(), bbox: map.getBounds() },
     });
-    elem.dispatchEvent(event);
+    window.dispatchEvent(event);
   });
 }
 
 export function setGeometryEvents(geometry, id) {
   geometry.on('click', (e) => {
-    const elem = document.getElementById('map');
     const event = new CustomEvent('pointclick', { detail: { point: e.latlng, rowId: id } });
-    elem.dispatchEvent(event);
+    window.dispatchEvent(event);
   });
 }
