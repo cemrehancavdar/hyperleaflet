59ae3c4dceeb458615b5bf8ccb349ab05004902c
cemrehancavdar <cemrehancavdar@hotmail.com>
2023-02-24 00:12:10 +0300

    fix: none no longer created debug json script

--- src/GeometryDebug.js
+++ src/GeometryDebug.js
@@ -21,29 +21,30 @@ const createGeometryDebugObject = (element = document) => {
 };
 
 export default function handleGeometryDisplay(addedNodes, removedNodes, displayStrategy) {
-  console.log(displayStrategy);
   if (displayStrategy === 'inline') {
-    return;
-  }
-
-  const { addToDebugObject, deleteFromDebugObject, saveDebugObject } = createGeometryDebugObject();
-
-  addedNodes.forEach((node) => {
-    if (node.nodeType === 1 && node.matches('[data-id]')) {
-      const { id, geometry, geometryType } = node.dataset;
-      node.removeAttribute('data-geometry');
-      if (displayStrategy === 'object') {
+    void 0;
+  } else if (displayStrategy === 'object') {
+    const { addToDebugObject, deleteFromDebugObject, saveDebugObject } = createGeometryDebugObject();
+    addedNodes.forEach((node) => {
+      if (node.nodeType === 1 && node.matches('[data-id]')) {
+        const { id, geometry, geometryType } = node.dataset;
+        node.removeAttribute('data-geometry');
         addToDebugObject(id, geometry, geometryType);
       }
-    }
-  });
+    });
 
-  removedNodes.forEach((node) => {
-    if (node.nodeType === 1 && node.matches('[data-id]')) {
-      const { id } = node.dataset;
-      deleteFromDebugObject(id);
-    }
-  });
-
-  saveDebugObject();
+    removedNodes.forEach((node) => {
+      if (node.nodeType === 1 && node.matches('[data-id]')) {
+        const { id } = node.dataset;
+        deleteFromDebugObject(id);
+      }
+    });
+    saveDebugObject();
+  } else if (displayStrategy === 'none') {
+    addedNodes.forEach((node) => {
+      if (node.nodeType === 1 && node.matches('[data-id]')) {
+        node.removeAttribute('data-geometry');
+      }
+    });
+  }
 }
