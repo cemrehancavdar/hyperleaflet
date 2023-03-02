a93984340577a4bf4d07fe8be538eca6497b6a92
cemrehancavdar <cemrehancavdar@hotmail.com>
2023-03-02 22:59:01 +0300

    test: tests on geometry click implemented

--- test/events.test.js
+++ test/events.test.js
@@ -1,6 +1,8 @@
 // // @vitest-environment happy-dom
 import { describe, it, expect, beforeEach, afterEach, vi } from 'vitest';
+import L from 'leaflet';
 import createHyperleafletMap from '../src/map-utils';
+import { setGeometryEvents } from '../src/events';
 
 describe('events', () => {
   beforeEach(() => {
@@ -69,5 +71,33 @@ describe('events', () => {
         }),
       }),
     );
+  });
+  describe('setGeometryEvents', () => {
+    beforeEach(() => {
+      document.body.innerHTML = `<div id="map" class="map" data-center="51.5074,-0.1278" data-zoom="10"> </div>`;
+    });
+
+    afterEach(() => {
+      document.body.innerHTML = '';
+    });
+    it('should dispatch a "pointclick" event when the geometry is clicked', () => {
+      const geometry = new L.Marker([51, 0]);
+      setGeometryEvents(geometry, '1');
+
+      const eventListener = vi.fn();
+      window.addEventListener('pointclick', eventListener);
+
+      geometry.fire('click');
+
+      expect(eventListener).toHaveBeenCalledWith(
+        expect.objectContaining({
+          type: 'pointclick',
+          detail: expect.objectContaining({
+            point: L.latLng(51, 0),
+            rowId: '1',
+          }),
+        }),
+      );
+    });
   });
 });
