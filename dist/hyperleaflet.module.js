caf76d0cf5ce1d78cf2b7c3b5b8dcca26f755656
cemrehancavdar <cemrehancavdar@hotmail.com>
2023-04-03 23:09:23 +0300

    build: build with 0.2.4

--- dist/hyperleaflet.module.js
+++ dist/hyperleaflet.module.js
@@ -1,2 +1,360 @@
-import{tileLayer as t,map as e,control as o,marker as n,GeoJSON as r,polyline as a,polygon as i,geoJSON as l}from"leaflet";var d={OpenStreetMap:t("https://tile.openstreetmap.org/{z}/{x}/{y}.png",{attribution:'&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'}),EsriWorldImagery:t("https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",{attribution:"Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"})};function p(t){var o,n=t.dataset,r=n.center,a=n.zoom,i={center:null!=(o=null==r?void 0:r.split(","))?o:[0,0],zoom:a||1};return function(t){return t.on("click",function(t){var e=new CustomEvent("mapclick",{detail:{point:t.latlng}});window.dispatchEvent(e)}),t.on("zoomend",function(){var e=new CustomEvent("mapzoom",{detail:{zoom:t.getZoom(),center:t.getCenter(),bbox:t.getBounds()}});window.dispatchEvent(e)}),t.on("move",function(){var e=new CustomEvent("mapmove",{detail:{zoom:t.getZoom(),center:t.getCenter(),bbox:t.getBounds()}});window.dispatchEvent(e)}),t}(e(t).setView(i.center,i.zoom))}var u=document.createElement("script");u.type="application/json",u.setAttribute("data-testid","debug"),u.innerText="{}",document.body.appendChild(u);var c=JSON.parse(u.text);function s(t){var e=t.dataset,o=e.id,n=e.geometry,r=e.geometryType;t.removeAttribute("data-geometry"),c[o]={type:r,coordinates:JSON.parse(n)},u.text=JSON.stringify(c,null,2)}function m(t){delete c[t.dataset.id],u.text=JSON.stringify(c,null,2)}function f(t){t.removeAttribute("data-geometry")}function v(){return v=Object.assign?Object.assign.bind():function(t){for(var e=1;e<arguments.length;e++){var o=arguments[e];for(var n in o)Object.prototype.hasOwnProperty.call(o,n)&&(t[n]=o[n])}return t},v.apply(this,arguments)}function y(t,e){t.on("click",function(){var o=new CustomEvent("pointclick",{detail:{point:t.getLatLng(),rowId:e}});window.dispatchEvent(o)})}var g=new Map,b=function(){var t=function(){var t=document.querySelector("#map"),e=p(t),n=function(t){var e=Array.from(t),n=e.map(function(t){var e=t.dataset,o=e.tile,n=e.maxZoom,r=d[o];return r?(r.options.minZoom=e.minZoom,r.options.maxZoom=n,r.name=o,{tile:r}):(console.warn(o+" is not in: \n"+Object.keys(d).join("\n")),null)}).filter(Boolean),r=function(t){var e=t.find(function(t){return"defaultTile"in t.dataset});return e?d[e.dataset.tile]:t.length?d[t[0].dataset.tile]:d.OpenStreetMap}(e);return{defaultHyperleafletTile:r,tileController:n.length?o.layers(Object.fromEntries(n.map(function(t){return[t.tile.name,t.tile]}))):null}}(t.querySelectorAll("[data-tile]")),r=n.defaultHyperleafletTile,a=n.tileController;return a&&a.addTo(e),r.addTo(e),e}();return function(t){var e=document.querySelector("[hyperleaflet]");if(e){var o=e.dataset.geometryDisplay||"object",l={};"object"===o?l={addCallback:s,removeCallback:m}:"remove"===o&&(l={addCallback:f,removeCallback:function(){}});var d=function(t,e){var o=e.addCallback,l=void 0===o?function(){}:o,d=e.removeCallback,p=void 0===d?function(){}:d;return{addNoteListToHyperleaflet:function(e){e.forEach(function(e){1===e.nodeType&&e.matches("[data-id]")&&(function(t){var e=t.dataset,o=e.id;if(o in g)return console.error("%c"+o,"color:red","already exists",t),[];var l,d,p,u,c,s,m=(d=(l=v({},e)).popup,p=l.tooltip,u=l.geometryType,c=l.id,s=JSON.parse(l.geometry),function(t){return function(e,o){switch(t){case"Point":return function(t,e){var o=n(t);return e.popup&&o.bindPopup(e.popup),e.tooltip&&o.bindTooltip(e.tooltip),y(o,e.id),o}(e,o);case"LineString":return function(t,e){var o=r.coordsToLatLngs(t,1),n=a(o);return e.popup&&n.bindPopup(e.popup),e.tooltip&&n.bindTooltip(e.tooltip),y(n,e.id),n}(e,o);case"Polygon":return function(t,e){var o=r.coordsToLatLngs(t,1),n=i(o);return e.popup&&n.bindPopup(e.popup),e.tooltip&&n.bindTooltip(e.tooltip),y(n,e.id),n}(e,o);default:return console.warn(t+" is not supported"),null}}}(u)(s,{popup:d,tooltip:p,id:c}));return g.set(o,m),[m]}(e)[0].addTo(t),l(e))})},removeNodeListToHyperleaflet:function(t){t.forEach(function(t){if(1===t.nodeType&&t.matches("[data-id]")){var e=function(t){var e=t.dataset.id,o=g.get(e);return g.delete(e),[o]}(t);e[0].remove(),p(t)}})}}}(t,l),p=d.addNoteListToHyperleaflet,u=d.removeNodeListToHyperleaflet;t.whenReady(function(){var t=e.querySelectorAll("[data-id]");p(t)}),new MutationObserver(function(t){t.forEach(function(t){"childList"===t.type&&(p(t.addedNodes),u(t.removedNodes))})}).observe(e,{childList:!0,subtree:!0,attributeFilter:["data-id"]})}}(t),{map:t,addGeoJsonToMap:function(e){l(e).addTo(t)}}}();export{b as default};
+import { tileLayer, map, control, marker, GeoJSON, polyline, polygon, geoJSON } from 'leaflet';
+
+var TILE_LAYERS = {
+  OpenStreetMap: tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }),
+  EsriWorldImagery: tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+    attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+  })
+};
+
+function setMapEvents(map) {
+  map.on('click', function (e) {
+    var event = new CustomEvent('mapclick', {
+      detail: {
+        point: e.latlng
+      }
+    });
+    window.dispatchEvent(event);
+  });
+  map.on('zoomend', function () {
+    var event = new CustomEvent('mapzoom', {
+      detail: {
+        zoom: map.getZoom(),
+        center: map.getCenter(),
+        bbox: map.getBounds()
+      }
+    });
+    window.dispatchEvent(event);
+  });
+  map.on('move', function () {
+    var event = new CustomEvent('mapmove', {
+      detail: {
+        zoom: map.getZoom(),
+        center: map.getCenter(),
+        bbox: map.getBounds()
+      }
+    });
+    window.dispatchEvent(event);
+  });
+  return map;
+}
+
+function getDefaultHyperleafletTile(tileLayerElementList) {
+  var defaultTileLayerElement = tileLayerElementList.find(function (t) {
+    return 'defaultTile' in t.dataset;
+  });
+  if (defaultTileLayerElement) {
+    return TILE_LAYERS[defaultTileLayerElement.dataset.tile];
+  }
+  if (tileLayerElementList.length) {
+    return TILE_LAYERS[tileLayerElementList[0].dataset.tile];
+  }
+  return TILE_LAYERS.OpenStreetMap;
+}
+function createHyperleafletTiles(tileLayerElementNodeList) {
+  var tileLayerElementList = Array.from(tileLayerElementNodeList);
+  var hyperleafletTiles = tileLayerElementList.map(function (tileLayerElement) {
+    var _tileLayerElement$dat = tileLayerElement.dataset,
+      tile = _tileLayerElement$dat.tile,
+      minZoom = _tileLayerElement$dat.minZoom,
+      maxZoom = _tileLayerElement$dat.maxZoom;
+    var currentTile = TILE_LAYERS[tile];
+    if (!currentTile) {
+      // eslint-disable-next-line no-console
+      console.warn(tile + " is not in: \n" + Object.keys(TILE_LAYERS).join('\n'));
+      return null;
+    }
+    currentTile.options.minZoom = minZoom;
+    currentTile.options.maxZoom = maxZoom;
+    currentTile.name = tile;
+    return {
+      tile: currentTile
+    };
+  }).filter(Boolean);
+  var defaultHyperleafletTile = getDefaultHyperleafletTile(tileLayerElementList);
+  return {
+    defaultHyperleafletTile: defaultHyperleafletTile,
+    tileController: hyperleafletTiles.length ? control.layers(Object.fromEntries(hyperleafletTiles.map(function (t) {
+      return [t.tile.name, t.tile];
+    }))) : null
+  };
+}
+function createHyperleafletMap(mapElement) {
+  var _center$split;
+  var _mapElement$dataset = mapElement.dataset,
+    center = _mapElement$dataset.center,
+    zoom = _mapElement$dataset.zoom,
+    minZoom = _mapElement$dataset.minZoom,
+    maxZoom = _mapElement$dataset.maxZoom;
+  var mapView = {
+    center: (_center$split = center == null ? void 0 : center.split(',')) != null ? _center$split : [0, 0],
+    zoom: zoom || 1
+  };
+  var leafletMap = map(mapElement, {
+    center: mapView.center,
+    zoom: mapView.zoom,
+    minZoom: minZoom || 0,
+    maxZoom: maxZoom || 18
+  });
+  return setMapEvents(leafletMap);
+}
+
+/**
+ *Creates a map
+ * @returns {L.Map}
+ */
+function createMap() {
+  var mapContainer = document.querySelector('#map');
+  var map = createHyperleafletMap(mapContainer);
+  var tileLayerElementList = mapContainer.querySelectorAll('[data-tile]');
+  var _createHyperleafletTi = createHyperleafletTiles(tileLayerElementList),
+    defaultHyperleafletTile = _createHyperleafletTi.defaultHyperleafletTile,
+    tileController = _createHyperleafletTi.tileController;
+  if (tileController) {
+    tileController.addTo(map);
+  }
+  defaultHyperleafletTile.addTo(map);
+  return map;
+}
+
+var debugMode = document.createElement('script');
+debugMode.type = 'application/json';
+debugMode.setAttribute('data-testid', 'debug');
+debugMode.innerText = '{}';
+document.body.appendChild(debugMode);
+var debugObject = JSON.parse(debugMode.text);
+function addToDebug(node) {
+  var _node$dataset = node.dataset,
+    id = _node$dataset.id,
+    geometry = _node$dataset.geometry,
+    geometryType = _node$dataset.geometryType;
+  node.removeAttribute('data-geometry');
+  debugObject[id] = {
+    type: geometryType,
+    coordinates: JSON.parse(geometry)
+  };
+  debugMode.text = JSON.stringify(debugObject, null, 2);
+}
+function deleteFromDebug(node) {
+  var id = node.dataset.id;
+  delete debugObject[id];
+  debugMode.text = JSON.stringify(debugObject, null, 2);
+}
+
+function utils(node) {
+  node.removeAttribute('data-geometry');
+}
+
+function _extends() {
+  _extends = Object.assign ? Object.assign.bind() : function (target) {
+    for (var i = 1; i < arguments.length; i++) {
+      var source = arguments[i];
+      for (var key in source) {
+        if (Object.prototype.hasOwnProperty.call(source, key)) {
+          target[key] = source[key];
+        }
+      }
+    }
+    return target;
+  };
+  return _extends.apply(this, arguments);
+}
+
+function setGeometryEvents(leafletObject, id) {
+  leafletObject.on('click', function () {
+    var event = new CustomEvent('pointclick', {
+      detail: {
+        point: leafletObject.getLatLng(),
+        rowId: id
+      }
+    });
+    window.dispatchEvent(event);
+  });
+}
+
+var createPointGeometry = function createPointGeometry(parsedGeometry, options) {
+  var geometry = marker(parsedGeometry);
+  if (options.popup) {
+    geometry.bindPopup(options.popup);
+  }
+  if (options.tooltip) {
+    geometry.bindTooltip(options.tooltip);
+  }
+  setGeometryEvents(geometry, options.id);
+  return geometry;
+};
+var createLineGeometry = function createLineGeometry(parsedGeometry, options) {
+  var flippedGeometry = GeoJSON.coordsToLatLngs(parsedGeometry, 1);
+  var geometry = polyline(flippedGeometry);
+  if (options.popup) {
+    geometry.bindPopup(options.popup);
+  }
+  if (options.tooltip) {
+    geometry.bindTooltip(options.tooltip);
+  }
+  setGeometryEvents(geometry, options.id);
+  return geometry;
+};
+var createPolygonGeometry = function createPolygonGeometry(parsedGeometry, options) {
+  var flippedGeometry = GeoJSON.coordsToLatLngs(parsedGeometry, 1);
+  var geometry = polygon(flippedGeometry);
+  if (options.popup) {
+    geometry.bindPopup(options.popup);
+  }
+  if (options.tooltip) {
+    geometry.bindTooltip(options.tooltip);
+  }
+  setGeometryEvents(geometry, options.id);
+  return geometry;
+};
+var createGeometry = function createGeometry(geometryType) {
+  return function (parsedGeometry, options) {
+    switch (geometryType) {
+      case 'Point':
+        return createPointGeometry(parsedGeometry, options);
+      case 'LineString':
+        return createLineGeometry(parsedGeometry, options);
+      case 'Polygon':
+        return createPolygonGeometry(parsedGeometry, options);
+      default:
+        // eslint-disable-next-line no-console
+        console.warn(geometryType + " is not supported");
+        return null;
+    }
+  };
+};
+function createLeafletObject(row) {
+  var geometry = row.geometry,
+    popup = row.popup,
+    tooltip = row.tooltip,
+    geometryType = row.geometryType,
+    id = row.id;
+  var parsedGeometry = JSON.parse(geometry);
+  var createGeometryFn = createGeometry(geometryType);
+  return createGeometryFn(parsedGeometry, {
+    popup: popup,
+    tooltip: tooltip,
+    id: id
+  });
+}
+
+var leafletObjectMap = new Map();
+function addNodeToHyperleaflet(node) {
+  var dataset = node.dataset;
+  var rowId = dataset.id;
+  if (rowId in leafletObjectMap) {
+    // eslint-disable-next-line no-console
+    console.error("%c" + rowId, 'color:red', 'already exists', node);
+    return [];
+  }
+  var leafletObject = createLeafletObject(_extends({}, dataset));
+  leafletObjectMap.set(rowId, leafletObject);
+  return [leafletObject];
+}
+function deleteNodeFromHyperleaflet(node) {
+  var rowId = node.dataset.id;
+  var leafletObject = leafletObjectMap.get(rowId);
+  leafletObjectMap["delete"](rowId);
+  return [leafletObject];
+}
+function hyperleafletGeometryHandler(map, _ref) {
+  var _ref$addCallback = _ref.addCallback,
+    addCallback = _ref$addCallback === void 0 ? function () {} : _ref$addCallback,
+    _ref$removeCallback = _ref.removeCallback,
+    removeCallback = _ref$removeCallback === void 0 ? function () {} : _ref$removeCallback;
+  var addNoteListToHyperleaflet = function addNoteListToHyperleaflet(nodes) {
+    nodes.forEach(function (node) {
+      if (node.nodeType === 1 && node.matches('[data-id]')) {
+        var _addNodeToHyperleafle = addNodeToHyperleaflet(node),
+          leafletObject = _addNodeToHyperleafle[0];
+        leafletObject.addTo(map);
+        addCallback(node);
+      }
+    });
+  };
+  function removeNodeListToHyperleaflet(nodes) {
+    nodes.forEach(function (node) {
+      if (node.nodeType === 1 && node.matches('[data-id]')) {
+        var _deleteNodeFromHyperl = deleteNodeFromHyperleaflet(node),
+          leafletObject = _deleteNodeFromHyperl[0];
+        leafletObject.remove();
+        removeCallback(node);
+      }
+    });
+  }
+  return {
+    addNoteListToHyperleaflet: addNoteListToHyperleaflet,
+    removeNodeListToHyperleaflet: removeNodeListToHyperleaflet
+  };
+}
+
+/**
+ * Adds the data from the hyperleaflet container to the map.
+ @param {L.Map} map
+ @param map
+ */
+
+function hyperleafletDataToMap(map) {
+  var hyperleafletDataContainer = document.querySelector('[hyperleaflet]');
+  if (!hyperleafletDataContainer) return;
+  var geometryDisplayStrategy = hyperleafletDataContainer.dataset.geometryDisplay || 'object';
+  var callbackFunctions = {};
+  if (geometryDisplayStrategy === 'object') {
+    callbackFunctions = {
+      addCallback: addToDebug,
+      removeCallback: deleteFromDebug
+    };
+  } else if (geometryDisplayStrategy === 'remove') {
+    callbackFunctions = {
+      addCallback: utils,
+      removeCallback: function removeCallback() {}
+    };
+  }
+  var _hyperleafletGeometry = hyperleafletGeometryHandler(map, callbackFunctions),
+    addNoteListToHyperleaflet = _hyperleafletGeometry.addNoteListToHyperleaflet,
+    removeNodeListToHyperleaflet = _hyperleafletGeometry.removeNodeListToHyperleaflet;
+  map.whenReady(function () {
+    var nodes = hyperleafletDataContainer.querySelectorAll('[data-id]');
+    addNoteListToHyperleaflet(nodes);
+  });
+  function callback(mutations) {
+    mutations.forEach(function (mutation) {
+      if (mutation.type === 'childList') {
+        addNoteListToHyperleaflet(mutation.addedNodes);
+        removeNodeListToHyperleaflet(mutation.removedNodes);
+      }
+    });
+  }
+  var observer = new MutationObserver(callback);
+  observer.observe(hyperleafletDataContainer, {
+    childList: true,
+    // observe direct children
+    subtree: true,
+    // and lower descendants too
+    attributeFilter: ['data-id']
+  });
+}
+
+var hyperleaflet = function hyperleaflet() {
+  var map = createMap();
+  hyperleafletDataToMap(map);
+
+  // TODO - move this to a separate file
+  /**
+   Adds a GeoJSON object to the map.
+   @param {Object} geoJSON - The GeoJSON object to add to the map.
+   @returns {void}
+   */
+  var addGeoJsonToMap = function addGeoJsonToMap(geoJSON$1) {
+    geoJSON(geoJSON$1).addTo(map);
+  };
+  return {
+    map: map,
+    addGeoJsonToMap: addGeoJsonToMap
+  };
+}();
+
+export { hyperleaflet as default };
 //# sourceMappingURL=hyperleaflet.module.js.map
