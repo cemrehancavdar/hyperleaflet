8de33aa294a1f95f6fd58d089253a65d90435e62
cemrehancavdar <cemrehancavdar@hotmail.com>
2023-05-25 23:57:54 +0300

    feature: hyperleaflet constantly controls if #map is there

--- src/Map/index.js
+++ src/Map/index.js
@@ -1,21 +1,39 @@
 import createHyperleafletMap, { createHyperleafletTiles } from './map';
+import hyperleafletDataToMap from '../Geometry/index';
 
-/**
- *Creates a map
- * @returns {L.Map}
- */
 function createMap() {
-  const mapContainer = document.querySelector('#map');
-  const map = createHyperleafletMap(mapContainer);
+  let initialized = false;
+  function initMap() {
+    const mapContainer = document.querySelector('#map');
+    if (mapContainer && !initialized) {
+      initialized = true;
+      const map = createHyperleafletMap(mapContainer);
 
-  const tileLayerElementList = mapContainer.querySelectorAll('[data-tile]');
-  const { defaultHyperleafletTile, tileController } = createHyperleafletTiles(tileLayerElementList);
+      const tileLayerElementList = mapContainer.querySelectorAll('[data-tile]');
+      const { defaultHyperleafletTile, tileController } = createHyperleafletTiles(tileLayerElementList);
 
-  if (tileController) {
-    tileController.addTo(map);
+      if (tileController) {
+        tileController.addTo(map);
+      }
+      defaultHyperleafletTile.addTo(map);
+      hyperleafletDataToMap(map);
+      window.hyperleaflet = { map };
+    }
   }
-  defaultHyperleafletTile.addTo(map);
-  return map;
-}
 
+  function observeMap() {
+    const observer = new MutationObserver(() => {
+      const mapElement = document.querySelector('#map');
+      if (mapElement) {
+        initMap();
+      } else if (initialized) {
+        initialized = false;
+        delete window.hyperleaflet;
+      }
+    });
+    observer.observe(document.documentElement, { childList: true, subtree: true });
+  }
+
+  return { initMap, observeMap };
+}
 export default createMap;
--- src/hyperleaflet.js
+++ src/hyperleaflet.js
@@ -1,23 +1,12 @@
-import { geoJSON as leafletGeoJSON } from 'leaflet';
-
 import createMap from './Map';
-import hyperleafletDataToMap from './Geometry';
 
 const hyperleaflet = (function hyperleaflet() {
-  const map = createMap();
-  hyperleafletDataToMap(map);
+  const { initMap, observeMap } = createMap();
 
-  // TODO - move this to a separate file
-  /**
-   Adds a GeoJSON object to the map.
-   @param {Object} geoJSON - The GeoJSON object to add to the map.
-   @returns {void}
-   */
-  const addGeoJsonToMap = (geoJSON) => {
-    leafletGeoJSON(geoJSON).addTo(map);
-  };
-
-  return { map, addGeoJsonToMap };
+  document.addEventListener('DOMContentLoaded', () => {
+    initMap();
+    observeMap();
+  });
 })();
 
 export default hyperleaflet;
