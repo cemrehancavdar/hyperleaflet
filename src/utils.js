a46e21be31e4f2dfdd7be0ed0d397efd714721ef
cemrehancavdar <cemrehancavdar@hotmail.com>
2024-01-24 01:06:33 +0300

    Fix mergeDeep function in utils.js

--- src/utils.js
+++ src/utils.js
@@ -7,8 +7,11 @@ export function mergeDeep(target, source) {
   if (isObject(target) && isObject(source)) {
     Object.keys(source).forEach((key) => {
       if (isObject(source[key])) {
-        if (!(key in target)) Object.assign(output, { [key]: source[key] });
+        if (!isObject(target[key])) Object.assign(output, { [key]: source[key] });
+        else if (!(key in target)) Object.assign(output, { [key]: source[key] });
         else output[key] = mergeDeep(target[key], source[key]);
+      } else if (isObject(source)) {
+        output[key] = source[key];
       } else {
         Object.assign(output, { [key]: source[key] });
       }
